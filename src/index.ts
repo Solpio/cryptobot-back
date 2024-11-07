11949fb00e2b191885a6d6100c4a816ee3b3f49b
Solpio <artemos267@gmail.com>
2024-11-07 15:01:53 -0300

    add auth by telegram

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,10 @@ bot.start().then(() => {
   console.log("Telegram Bot запущен!");
 });
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: +(process.env.PORT || 8080) }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`Server listening at ${address}`);
 });
-
-// console.log('Happy developing ✨')
