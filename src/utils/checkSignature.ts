bb5498bce37460e264697ad2439e2ca99405593c
Solpio <artemos267@gmail.com>
2024-11-09 16:28:53 -0300

    error build fix

--- src/utils/checkSignature.ts
+++ src/utils/checkSignature.ts
@@ -1,6 +1,9 @@
 import { createHash, createHmac } from "crypto";
 
-const checkSignature = (token, { body, headers }) => {
+const checkSignature = (
+  token: string,
+  { body, headers }: { body: any; headers: any },
+) => {
   const secret = createHash("sha256").update(token).digest();
   const checkString = JSON.stringify(body);
   const hmac = createHmac("sha256", secret).update(checkString).digest("hex");
